d2d389d7f97d03079891396427a31aa7d68d654e
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-09-17 20:07:11 -0300

    Criei um contador de caracteres

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const senha = document.getElementsByClassName('senha');
 const botaoLogin = document.getElementsByClassName('botao-login');
 const botaoEnviar = document.getElementById('submit-btn');
 const chekcboxInfo = document.getElementById('agreement');
+const textArea = document.getElementById('textarea');
+const contador = document.getElementById('counter');
 
 function validacaoLogin() {
   if (email[0].value === 'tryber@teste.com' && senha[0].value === '123456') {
@@ -26,5 +28,14 @@ function habilitarBotao() {
   // botaoEnviar.disabled = !checkboxInfo.checked; (outra opção)
 }
 
+function contadorCaracteres(evento) {
+  const escrevendo = evento.target;
+  const digitacao = escrevendo.value.length;
+  console.log(digitacao);
+  contador.innerText = textArea.getAttribute('maxlength') - digitacao;
+}
+
 botaoLogin[0].addEventListener('click', enviarLogin);
 chekcboxInfo.addEventListener('change', habilitarBotao);
+textArea.addEventListener('keyup', contadorCaracteres);
+// quando usei o keydown, quando clicava no delete ele incrementava uma vez e depois ia decrementando a quantidade de caracteres, aí não dava certo. O keydown interfere no length, pois independente da tecla que eu digito, ele conta um caractere a mais ou a menos. Com o keyup, o length permanece igual para teclas como numlock, alt, shift, ctrl etc.

9e7b96631e7c076ec67dc315b7b6c7d6465e1b68
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-09-17 11:32:55 -0300

    Habilitei o botão Enviar após a validação do checkbox

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementsByClassName('email');
 const senha = document.getElementsByClassName('senha');
 const botaoLogin = document.getElementsByClassName('botao-login');
+const botaoEnviar = document.getElementById('submit-btn');
+const chekcboxInfo = document.getElementById('agreement');
 
 function validacaoLogin() {
   if (email[0].value === 'tryber@teste.com' && senha[0].value === '123456') {
@@ -17,4 +19,12 @@ function enviarLogin() {
   }
 }
 
+function habilitarBotao() {
+  if (chekcboxInfo.checked) {
+    botaoEnviar.disabled = false;
+  }
+  // botaoEnviar.disabled = !checkboxInfo.checked; (outra opção)
+}
+
 botaoLogin[0].addEventListener('click', enviarLogin);
+chekcboxInfo.addEventListener('change', habilitarBotao);
